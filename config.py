e4e595daeb47b07dab743d12bb1fe9c5b5cd15f7
pwbhairepo11 <208841583+pwbhairepo11@users.noreply.github.com>
2025-09-13 23:29:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import getenv
 
 API_ID = int(os.environ.get("API_ID", "28460032"))  # Replace "123456" with your actual api_id or use .env
 API_HASH = os.environ.get("API_HASH", "1457c3ba64719a1e442aae67217b67c2")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8150025209:AAHstLep9sQePj2X0YvitaXfgmz8EvKsGY0")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 OWNER_ID = int(os.environ.get("OWNER_ID", "6977768796"))  # Your Telegram user ID
 SUDO_USERS = list(map(int, os.environ.get("SUDO_USERS", "").split()))  # Space-separated user IDs
